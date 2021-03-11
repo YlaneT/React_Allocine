703d9d2a12c02c6de7e33628d757660e084c6c58
YlaneT <yloo318@yahoo.fr>
2021-03-11 12:26:16 +0100

    Gestion de l'authentification avec 1 useState pour chaque param

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Routes from "./config/router"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      
+        <Routes/>        
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -18,7 +18,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      
     </div>
   );
 }
