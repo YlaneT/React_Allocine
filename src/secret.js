a8fd69d07fe3b576d3808e3888c3232d2c70f832
YlaneT <yloo318@yahoo.fr>
2021-03-28 15:57:45 +0200

    Ajout .secret.js

---
+++ src/secret.js
@@ -0,0 +1,8 @@
+const apikey = 'k_qjztqrcj'
+// const apikey = 'k_2tm9qnvd'
+// const apikey = 'k_ov9a6030'
+// const apikey = 'k_o1uizwe3'
+
+const url_base = 'https://imdb-api.com/'
+
+export {apikey,url_base} ;
